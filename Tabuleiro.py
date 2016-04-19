fa898eccd064631627add87007f97f87d267e039
Gabriel Noal <gabriel.oliva@hotmail.com>
2016-04-19 09:20:13 -0300

    Adicionei os link de Tkinter q o fabio passou e comecei a fazer o tabuleiro ;)

---
+++ Tabuleiro.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 08:45:35 2016
+
+@author: Usuario
+"""
+
+import tkinter as tk
+
+janela = tk.Tk()
+botao_0_1= tk.Button(janela)
+botao_0_1.grid()
+janela.mainloop()
