edfe1cf320f38d6e5fe5c9fa6a840da012ac539b
Gabriel Noal <gabriel.oliva@hotmail.com>
2016-04-25 15:35:51 -0300

    Vamo que vamo!!!

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -32,35 +32,35 @@ class Tabuleiro:
         self.botao_0_0.grid(row=0, column=0, stick="nsew")
         
         self.botao_0_1 = tk.Button(self.janela)
-        self.botao_0_1.configure(command=Jogo.recebe_jogada(0,1))
+        self.botao_0_1.configure(command= self.botao_0_1_clicado)
         self.botao_0_1.grid(row=0, column=1, stick="nsew")
 
         self.botao_0_2 = tk.Button(self.janela)
-        self.botao_0_2.configure(command=Jogo.recebe_jogada(0,2))
+        self.botao_0_2.configure(command= self.botao_0_2_clicado)
         self.botao_0_2.grid(row=0, column=2, stick="nsew")
         
         self.botao_1_0 = tk.Button(self.janela)
-        self.botao_1_0.configure(command=Jogo.recebe_jogada(1,0))
+        self.botao_1_0.configure(command= self.botao_1_0_clicado)
         self.botao_1_0.grid(row=1, column=0, stick="nsew")
 
         self.botao_1_1 = tk.Button(self.janela)
-        self.botao_1_1.configure(command=Jogo.recebe_jogada(1,1))
+        self.botao_1_1.configure(command= self.botao_1_1_clicado)
         self.botao_1_1.grid(row=1, column=1, stick="nsew")
         
         self.botao_1_2 = tk.Button(self.janela)
-        self.botao_1_2.configure(command=Jogo.recebe_jogada(1,2))
+        self.botao_1_2.configure(command= self.botao_1_2_clicado)
         self.botao_1_2.grid(row=1, column=2, stick="nsew")
         
         self.botao_2_0 = tk.Button(self.janela)
-        self.botao_2_0.configure(command=Jogo.recebe_jogada(2,0))
+        self.botao_2_0.configure(command= self.botao_2_0_clicado)
         self.botao_2_0.grid(row=2, column=0, stick="nsew")
         
         self.botao_2_1 = tk.Button(self.janela)
-        self.botao_2_1.configure(command=Jogo.recebe_jogada(2,1))
+        self.botao_2_1.configure(command= self.botao_2_1_clicado)
         self.botao_2_1.grid(row=2, column=1, stick="nsew")
 
         self.botao_2_2 = tk.Button(self.janela)
-        self.botao_2_2.configure(command=Jogo.recebe_jogada(2,2))
+        self.botao_2_2.configure(command= self.botao_2_2_clicado)
         self.botao_2_2.grid(row=2, column=2, stick="nsew")
                 
         
@@ -74,8 +74,86 @@ class Tabuleiro:
         Jogo.recebe_jogada(0,0)
         self.botao_0_0.configure(state="disable")
         
+    def botao_0_1_clicado(self):
+        if Jogo.player == 1:
+            self.botao_0_1.configure(text= "X")
+            self.botao_0_1.configure(font = "Arial 10")
+        else:
+            self.botao_0_1.configure(text= "O")
+            self.botao_0_1.configure(font = "Arial 10")
+        Jogo.recebe_jogada(0,1)
+        self.botao_0_1.configure(state="disable")
         
+    def botao_0_2_clicado(self):
+        if Jogo.player == 1:
+            self.botao_0_2.configure(text= "X")
+            self.botao_0_2.configure(font = "Arial 10")
+        else:
+            self.botao_0_2.configure(text= "O")
+            self.botao_0_2.configure(font = "Arial 10")
+        Jogo.recebe_jogada(0,2)
+        self.botao_0_2.configure(state="disable")
         
+    def botao_1_0_clicado(self):
+        if Jogo.player == 1:
+            self.botao_1_0.configure(text= "X")
+            self.botao_1_0.configure(font = "Arial 10")
+        else:
+            self.botao_1_0.configure(text= "O")
+            self.botao__1.configure(font = "Arial 10")
+        Jogo.recebe_jogada(1,0)
+        self.botao_1_0.configure(state="disable")
+    
+    def botao_1_1_clicado(self):
+        if Jogo.player == 1:
+            self.botao_1_1.configure(text= "X")
+            self.botao_1_1.configure(font = "Arial 10")
+        else:
+            self.botao_1_1.configure(text= "O")
+            self.botao_1_1.configure(font = "Arial 10")
+        Jogo.recebe_jogada(1,1)
+        self.botao_1_1.configure(state="disable")
+    
+    def botao_1_2_clicado(self):
+        if Jogo.player == 1:
+            self.botao_1_2.configure(text= "X")
+            self.botao_1_2.configure(font = "Arial 10")
+        else:
+            self.botao_1_2.configure(text= "O")
+            self.botao_1_2.configure(font = "Arial 10")
+        Jogo.recebe_jogada(2,0)
+        self.botao_1_2.configure(state="disable")
+    
+    def botao_2_0_clicado(self):
+        if Jogo.player == 1:
+            self.botao_2_0.configure(text= "X")
+            self.botao_2_0.configure(font = "Arial 10")
+        else:
+            self.botao_2_0.configure(text= "O")
+            self.botao_2_0.configure(font = "Arial 10")
+        Jogo.recebe_jogada(2,0)
+        self.botao_2_0.configure(state="disable")
+    
+    def botao_2_1_clicado(self):
+        if Jogo.player == 1:
+            self.botao_2_1.configure(text= "X")
+            self.botao_2_1.configure(font = "Arial 10")
+        else:
+            self.botao_2_1.configure(text= "O")
+            self.botao_2_1.configure(font = "Arial 10")
+        Jogo.recebe_jogada(2,1)
+        self.botao_2_1.configure(state="disable")
+    
+    def botao_2_2_clicado(self):
+        if Jogo.player == 1:
+            self.botao_2_2.configure(text= "X")
+            self.botao_2_2.configure(font = "Arial 10")
+        else:
+            self.botao_2_2.configure(text= "O")
+            self.botao_2_2.configure(font = "Arial 10")
+        Jogo.recebe_jogada(2,2)
+        self.botao_2_2.configure(state="disable")
+    
     def iniciar(self):
         self.janela.mainloop()
         
